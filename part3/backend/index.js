c18e520d29beca201929c2e45b9c69ad176e54ac
ojitxslml <jp.urra42@gmail.com>
2024-09-22 21:48:35 -0300

    deploy into fly.io

--- part3/backend/index.js
+++ part3/backend/index.js
@@ -1,6 +1,9 @@
 const express = require('express')
+const cors = require('cors')
 const app = express()
 app.use(express.json())
+app.use(cors())
+app.use(express.static('dist'))
 let notes = [
   {
     id: "1",
@@ -73,7 +76,7 @@ app.post('/api/notes', (request, response) => {
   response.json(note)
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
