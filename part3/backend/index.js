4b486a9065aa824780527f26ff1a80b6c9cd0399
ojitxslml <jp.urra42@gmail.com>
2024-09-28 21:46:24 -0300

    Part 3 C before route handlers

--- part3/backend/index.js
+++ part3/backend/index.js
@@ -1,29 +1,16 @@
 const express = require('express')
 const cors = require('cors')
+require('dotenv').config();
 const app = express()
 app.use(express.json())
 app.use(cors())
 app.use(express.static('dist'))
-let notes = [
-  {
-    id: "1",
-    content: "HTML is easy",
-    important: true
-  },
-  {
-    id: "2",
-    content: "Browser can execute only JavaScript",
-    important: false
-  },
-  {
-    id: "3",
-    content: "GET and POST are the most important methods of HTTP protocol",
-    important: true
-  }
-]
+const Note = require('./models/note')
 
-app.get('/', (request, response) => {
-  response.send('<h1>Hello World!</h1>')
+app.get('/api/notes', (request, response) => {
+  Note.find({}).then(notes => {
+    response.json(notes)
+  })
 })
 
 app.get('/api/notes', (request, response) => {
@@ -76,7 +63,7 @@ app.post('/api/notes', (request, response) => {
   response.json(note)
 })
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
