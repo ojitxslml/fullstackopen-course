121d180b39e607dab575cdd44f7085a51e6f652e
ojitxslml <jp.urra42@gmail.com>
2024-10-11 01:29:39 -0300

    Exercises 4.15.-4.23

--- part4/bloglist_backend/tests/blog_api.test.js
+++ part4/bloglist_backend/tests/blog_api.test.js
@@ -6,14 +6,57 @@ const app = require("../app");
 const helper = require("./test_helper");
 const api = supertest(app);
 const Blog = require("../models/blog");
+const User = require("../models/user");
+const jwt = require("jsonwebtoken");
+
+let token; // Variable para almacenar el token
 
 beforeEach(async () => {
   await Blog.deleteMany({});
+  await User.deleteMany({}); // Asegúrate de limpiar la colección de usuarios antes de las pruebas
 
-  for (let blog of helper.initialBlogs) {
-    let blogObject = new Blog(blog);
-    await blogObject.save();
-  }
+  // Crea un usuario y obtén su token
+  const newUser = {
+    username: "testuser2",
+    name: "Test User 2",
+    password: "password2",
+  };
+
+  await api.post("/api/users").send(newUser); // Asegúrate de que el endpoint de usuarios esté disponible
+
+  const response = await api
+    .post("/api/login")
+    .send({ username: newUser.username, password: newUser.password });
+
+  assert(response.body.token, "Token was not received");
+  token = response.body.token;
+
+  // Crea dos blogs de prueba
+  const blog1 = {
+    title: "Blog de prueba 1",
+    author: "Autor 1",
+    url: "http://example.com/blog1",
+    likes: 10,
+  };
+
+  const blog2 = {
+    title: "Blog de prueba 2",
+    author: "Autor 2",
+    url: "http://example.com/blog2",
+    likes: 5,
+  };
+
+  await api
+    .post("/api/blogs")
+    .set("Authorization", `Bearer ${token}`)
+    .send(blog1)
+    .expect(201);
+
+  await api
+    .post("/api/blogs")
+    .set("Authorization", `Bearer ${token}`)
+    .send(blog2)
+    .expect(201);
 });
 
 test("blogs are returned as json", async () => {
@@ -50,16 +93,30 @@ test("a new blog can be created", async () => {
 
   await api
     .post("/api/blogs")
+    .set("Authorization", `Bearer ${token}`) // Agregar el token
     .send(newBlog)
-    .expect(201) //created code
+    .expect(201) // Código 201: creado
     .expect("Content-Type", /application\/json/);
 
-  // blogs are higher
+  // blogs son mayores
   const blogsAtEnd = await helper.blogsInDb();
   assert.strictEqual(blogsAtEnd.length, helper.initialBlogs.length + 1);
 
   const titles = blogsAtEnd.map((b) => b.title);
   assert(titles.includes("New Blog Title"));
+});
+
+test("fails with status 401 if no token is provided", async () => {
+  const newBlog = {
+    title: "Blog without token",
+    author: "Author Name",
+    url: "http://example.com",
+    likes: 5,
+  };
+
+  const response = await api.post("/api/blogs").send(newBlog).expect(401); // Código 401: no autorizado
+
+  assert.strictEqual(response.body.error, "token missing");
 });
 
 test("likes default to 0 if missing", async () => {
@@ -72,11 +129,12 @@ test("likes default to 0 if missing", async () => {
 
   const response = await api
     .post("/api/blogs")
+    .set("Authorization", `Bearer ${token}`) // Agregar el token
     .send(newBlog)
     .expect(201)
     .expect("Content-Type", /application\/json/);
 
-  // likes is 0
+  // likes es 0
   assert.strictEqual(response.body.likes, 0);
 });
 
@@ -104,12 +162,13 @@ test("responds with status 400 if url is missing", async () => {
   assert.strictEqual(response.body.error, "title or url missing");
 });
 
-test('a blog can be deleted', async () => {
+test("a blog can be deleted", async () => {
   const blogsAtStart = await helper.blogsInDb();
   const blogToDelete = blogsAtStart[0];
 
   await api
     .delete(`/api/blogs/${blogToDelete.id}`)
+    .set("Authorization", `Bearer ${token}`)
     .expect(204);
 
   const blogsAtEnd = await helper.blogsInDb();
@@ -119,16 +178,17 @@ test('a blog can be deleted', async () => {
   assert(!titles.includes(blogToDelete.title));
 });
 
-test('a blog can be updated', async () => {
+test("a blog can be updated", async () => {
   const blogsAtStart = await helper.blogsInDb();
   const blogToUpdate = blogsAtStart[0];
 
   const updatedData = {
-    likes: blogToUpdate.likes + 1
+    likes: blogToUpdate.likes + 1,
   };
 
   const response = await api
     .put(`/api/blogs/${blogToUpdate.id}`)
+    .set("Authorization", `Bearer ${token}`) // Agregar el token
     .send(updatedData)
     .expect(200)
     .expect("Content-Type", /application\/json/);
@@ -136,10 +196,9 @@ test('a blog can be updated', async () => {
   assert.strictEqual(response.body.likes, updatedData.likes);
 
   const blogsAtEnd = await helper.blogsInDb();
-  const updatedBlog = blogsAtEnd.find(b => b.id === blogToUpdate.id);
+  const updatedBlog = blogsAtEnd.find((b) => b.id === blogToUpdate.id);
   assert.strictEqual(updatedBlog.likes, updatedData.likes);
 });
-
 
 after(async () => {
   await mongoose.connection.close();
