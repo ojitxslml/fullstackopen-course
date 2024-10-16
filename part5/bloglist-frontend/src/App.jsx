d866143ea1db851891a0c30648584cb750c341a5
ojitxslml <jp.urra42@gmail.com>
2024-10-16 00:10:15 -0300

    Exercises 5.5.-5.11

--- part5/bloglist-frontend/src/App.jsx
+++ part5/bloglist-frontend/src/App.jsx
@@ -1,23 +1,23 @@
 import { useState, useEffect, useRef } from "react";
 import Blog from "./components/Blog";
-import Notification from './components/Notification'
+import Notification from "./components/Notification";
 import blogService from "./services/blogs";
 import loginService from "./services/login";
-import LoginForm from './components/LoginForm'
-import Togglable from './components/Togglable'
-import BlogForm from './components/BlogForm'
+import LoginForm from "./components/LoginForm";
+import Togglable from "./components/Togglable";
+import BlogForm from "./components/BlogForm";
 
 const App = () => {
   const [blogs, setBlogs] = useState([]);
-  
+
   const [notificationMessage, setNotificationMessage] = useState(null);
   const [notificationType, setNotificationType] = useState(null);
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
-  const [user, setUser] = useState(null)
-  const [loginVisible, setLoginVisible] = useState(false)
+  const [user, setUser] = useState(null);
+  const [loginVisible, setLoginVisible] = useState(false);
 
-  const BlogFormRef = useRef()
+  const BlogFormRef = useRef();
 
   useEffect(() => {
     blogService.getAll().then((blogs) => setBlogs(blogs));
@@ -31,8 +31,6 @@ const App = () => {
       blogService.setToken(user.token);
     }
   }, []);
-
-
 
   const handleLogin = async (event) => {
     event.preventDefault();
@@ -62,7 +60,9 @@ const App = () => {
     try {
       const returnedBlog = await blogService.create(blogObject);
       setBlogs(blogs.concat(returnedBlog));
-      setNotificationMessage(`a new blog ${blogObject.title} by ${blogObject.author} added`);
+      setNotificationMessage(
+        `a new blog ${blogObject.title} by ${blogObject.author} added`
+      );
       setNotificationType("successful");
       setTimeout(() => {
         setNotificationMessage(null);
@@ -78,9 +78,21 @@ const App = () => {
     }
   };
 
+  const updateBlogList = (updatedBlogOrId) => {
+    if (typeof updatedBlogOrId === "string") {
+      setBlogs(blogs.filter((blog) => blog.id !== updatedBlogOrId));
+    } else {
+      setBlogs(
+        blogs.map((blog) =>
+          blog.id === updatedBlogOrId.id ? updatedBlogOrId : blog
+        )
+      );
+    }
+  };
+
   const loginForm = () => {
-    const hideWhenVisible = { display: loginVisible ? 'none' : '' }
-    const showWhenVisible = { display: loginVisible ? '' : 'none' }
+    const hideWhenVisible = { display: loginVisible ? "none" : "" };
+    const showWhenVisible = { display: loginVisible ? "" : "none" };
 
     return (
       <div>
@@ -98,32 +110,38 @@ const App = () => {
           <button onClick={() => setLoginVisible(false)}>cancel</button>
         </div>
       </div>
-    )
-  }
+    );
+  };
 
   const logout = () => {
     window.localStorage.clear();
     window.location.reload();
   };
-
+  const sortedBlogs = blogs.sort((a, b) => b.likes - a.likes);
   return (
     <div>
       <h2>blogs</h2>
 
       <Notification message={notificationMessage} type={notificationType} />
 
-{!user && loginForm()}
-{user && <div>
-       <p>{user.name} logged in <button onClick={logout}>logout</button></p>
-       <Togglable buttonLabel='new note' ref={BlogFormRef}>
-        <BlogForm
-          createBlog={addBlog}
+      {!user && loginForm()}
+      {user && (
+        <div>
+          <p>
+            {user.name} logged in <button onClick={logout}>logout</button>
+          </p>
+          <Togglable buttonLabel="new note" ref={BlogFormRef}>
+            <BlogForm createBlog={addBlog} />
+          </Togglable>
+        </div>
+      )}
+      {sortedBlogs.map((blog) => (
+        <Blog
+          key={blog.id}
+          blog={blog}
+          user={user}
+          updateBlogList={updateBlogList}
         />
-      </Togglable>
-      </div>
-     } 
-      {blogs.map((blog) => (
-        <Blog key={blog.id} blog={blog} />
       ))}
     </div>
   );
--- part5/bloglist-frontend/src/components/Blog.jsx
+++ part5/bloglist-frontend/src/components/Blog.jsx
@@ -1,7 +1,73 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from "react";
+import blogService from "../services/blogs";
 
-export default Blog
+const Blog = ({ blog, user, updateBlogList }) => {
+  const [visible, setVisible] = useState(false);
+
+  const toggleVisibility = () => {
+    setVisible(!visible);
+  };
+
+  const blogStyle = {
+    padding: 4,
+    border: "solid",
+    borderWidth: 1,
+    marginBottom: 4,
+  };
+
+  const handleLike = async () => {
+    const updatedBlog = {
+      ...blog,
+      likes: blog.likes + 1,
+    };
+
+    try {
+      const returnedBlog = await blogService.update(blog.id, updatedBlog);
+      updateBlogList(returnedBlog);
+    } catch (error) {
+      console.error("Error updating likes", error);
+    }
+  };
+
+  const handleDelete = async () => {
+    const confirmDeletion = window.confirm(
+      `Remove blog "${blog.title}" by ${blog.author}?`
+    );
+    if (confirmDeletion) {
+      try {
+        await blogService.remove(blog.id);
+        updateBlogList(blog.id);
+      } catch (error) {
+        console.error("Error deleting blog:", error);
+      }
+    }
+  };
+
+  return (
+    <div style={blogStyle}>
+      <div>
+        {blog.title} {blog.author}
+        <button onClick={toggleVisibility}>{visible ? "hide" : "view"}</button>
+      </div>
+      {visible && (
+        <div>
+          <p>{blog.url}</p>
+          <p>
+            likes {blog.likes} <button onClick={handleLike}>like</button>
+          </p>
+          <p>{blog.author}</p>
+          {user && user.username === blog.user.username && (
+            <button
+              onClick={handleDelete}
+              style={{ marginLeft: "10px", color: "red" }}
+            >
+              delete
+            </button>
+          )}
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Blog;
