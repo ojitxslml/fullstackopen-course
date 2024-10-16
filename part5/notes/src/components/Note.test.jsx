6ac462dce21824370ecbd347f91487286d704518
ojitxslml <jp.urra42@gmail.com>
2024-10-16 17:11:32 -0300

    part 5.c

---
+++ part5/notes/src/components/Note.test.jsx
@@ -0,0 +1,63 @@
+import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+import Togglable from './Togglable'
+import Note from './Note'
+
+describe('<Togglable />', () => {
+  let container
+
+  beforeEach(() => {
+    container = render(
+      <Togglable buttonLabel="show...">
+        <div className="testDiv" >
+          togglable content
+        </div>
+      </Togglable>
+    ).container
+  })
+
+  test('renders its children', async () => {
+    await screen.findAllByText('togglable content')
+  })
+
+  test('at start the children are not displayed', () => {
+    const div = container.querySelector('.togglableContent')
+    expect(div).toHaveStyle('display: none')
+  })
+
+  test('after clicking the button, children are displayed', async () => {
+    const user = userEvent.setup()
+    const button = screen.getByText('show...')
+    await user.click(button)
+
+    const div = container.querySelector('.togglableContent')
+    expect(div).not.toHaveStyle('display: none')
+  })
+})
+
+test('renders content', () => {
+    const note = {
+      content: 'Does not work anymore :(',
+      important: true
+    }
+  
+    render(<Note note={note} />)
+  
+    const element = screen.getByText(
+        'Does not work anymore :(', { exact: false }
+      )
+  
+    expect(element).toBeDefined()
+  })
+
+  test('does not render this', () => {
+    const note = {
+      content: 'This is a reminder',
+      important: true
+    }
+  
+    render(<Note note={note} />)
+  
+    const element = screen.queryByText('do not want this thing to be rendered')
+    expect(element).toBeNull()
+  })
