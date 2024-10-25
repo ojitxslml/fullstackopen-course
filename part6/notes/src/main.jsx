975120a912b76e2bae26541453084891e3e810de
ojitxslml <jp.urra42@gmail.com>
2024-10-24 22:18:53 -0300

    part 6.a

--- part6/notes/src/main.jsx
+++ part6/notes/src/main.jsx
@@ -1,53 +1,17 @@
 import ReactDOM from 'react-dom/client'
-import noteReducer from './reducers/noteReducer'
 import { createStore } from 'redux'
+
+import { Provider } from 'react-redux'
+
+import App from './App'
+import noteReducer from './reducers/noteReducer'
 
 const store = createStore(noteReducer)
 
-store.dispatch({
-  type: 'NEW_NOTE',
-  payload: {
-    content: 'the app state is in redux store',
-    important: true,
-    id: 1
-  }
-})
+ReactDOM.createRoot(document.getElementById('root')).render(
 
-store.dispatch({
-    type: 'TOGGLE_IMPORTANCE',
-    payload: {
-      id: 2
-    }
-})
+  <Provider store={store}>
+    <App />
 
-store.dispatch({
-  type: 'NEW_NOTE',
-  payload: {
-    content: 'state changes are made with actions',
-    important: false,
-    id: 2
-  }
-})
-
-const App = () => {
-  return(
-    <div>
-      <ul>
-        {store.getState().map(note=>
-          <li key={note.id}>
-            {note.content} <strong>{note.important ? 'important' : ''}</strong>
-          </li>
-        )}
-        </ul>
-    </div>
-  )
-}
-
-const root = ReactDOM.createRoot(document.getElementById('root'))
-
-const renderApp = () => {
-  root.render(<App />)
-}
-
-renderApp()
-store.subscribe(renderApp)
+  </Provider>
+)
