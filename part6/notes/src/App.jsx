a43600c0086b080519b1ea5211c23b36b8096b52
ojitxslml <jp.urra42@gmail.com>
2024-10-26 19:00:48 -0300

    part 6.b

--- part6/notes/src/App.jsx
+++ part6/notes/src/App.jsx
@@ -1,14 +1,15 @@
-import NewNote from './components/NewNote'
 import Notes from './components/Notes'
+import NewNote from './components/NewNote'
+import VisibilityFilter from './components/VisibilityFilter'
 
 const App = () => {
-
-    return (
-      <div>
-        <NewNote />
-        <Notes />
-      </div>
-    )
-  }
+  return (
+    <div>
+      <NewNote />
+      <VisibilityFilter />
+      <Notes />
+    </div>
+  )
+}
 
 export default App
--- part6/notes/src/main.jsx
+++ part6/notes/src/main.jsx
@@ -1,17 +1,34 @@
 import ReactDOM from 'react-dom/client'
-import { createStore } from 'redux'
 
-import { Provider } from 'react-redux'
-
+import { createStore, combineReducers } from 'redux'
+import { Provider } from 'react-redux' 
 import App from './App'
+
 import noteReducer from './reducers/noteReducer'
 
-const store = createStore(noteReducer)
+import filterReducer from './reducers/filterReducer'
+
+import { createNote } from './reducers/noteReducer'
+import { filterChange } from './reducers/filterReducer'
+const reducer = combineReducers({
+  notes: noteReducer,
+  filter: filterReducer
+})
+
+
+const store = createStore(reducer)
+
+
+
+
+console.log(store.getState())
+
+store.subscribe(() => console.log(store.getState()))
+store.dispatch(filterChange('IMPORTANT'))
+store.dispatch(createNote('combineReducers forms one reducer from many simple reducers'))
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-
   <Provider store={store}>
     <App />
-
   </Provider>
 )
