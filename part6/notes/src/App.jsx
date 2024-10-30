655b3a0343d438cc78607b3279fb9196b60f555f
ojitxslml <jp.urra42@gmail.com>
2024-10-30 11:03:13 -0300

    part 6.c

--- part6/notes/src/App.jsx
+++ part6/notes/src/App.jsx
@@ -7,13 +7,15 @@ import noteService from './services/notes'
 import { setNotes } from './reducers/noteReducer'
 import { useDispatch } from 'react-redux'
 
+import { initializeNotes } from './reducers/noteReducer'
+
 const App = () => {
 
   const dispatch = useDispatch()
+
   useEffect(() => {
-    noteService
-      .getAll().then(notes => dispatch(setNotes(notes)))
-  }, [])
+    dispatch(initializeNotes()) 
+  }, []) 
 
   return (
     <div>
