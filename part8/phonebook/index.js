2ed388ee19dd3fae1a006ac94819e3c6e87b3303
ojitxslml <jp.urra42@gmail.com>
2024-11-21 18:34:14 -0300

    part 8.e

--- part8/phonebook/index.js
+++ part8/phonebook/index.js
@@ -1,29 +1,32 @@
 const { ApolloServer } = require('@apollo/server')
-
-const { expressMiddleware } = require('@apollo/server/express4')
 const { ApolloServerPluginDrainHttpServer } = require('@apollo/server/plugin/drainHttpServer')
+const { expressMiddleware } = require('@apollo/server/express4')
 const { makeExecutableSchema } = require('@graphql-tools/schema')
-const express = require('express')
-const cors = require('cors')
+
+const { WebSocketServer } = require('ws')
+const { useServer } = require('graphql-ws/lib/use/ws')
+
 const http = require('http')
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
 
 const jwt = require('jsonwebtoken')
-
 const mongoose = require('mongoose')
-
+mongoose.set('strictQuery', false)
+const Person = require('./models/person')
 const User = require('./models/user')
-
-require("dotenv").config();
 
 const typeDefs = require('./schema')
 const resolvers = require('./resolvers')
 
-const MONGODB_URI = process.env.MONGODB_URI;
+require('dotenv').config()
+
+const MONGODB_URI = process.env.MONGODB_URI
 
 console.log('connecting to', MONGODB_URI)
 
-mongoose
-  .connect(MONGODB_URI)
+mongoose.connect(MONGODB_URI)
   .then(() => {
     console.log('connected to MongoDB')
   })
@@ -31,14 +34,34 @@ mongoose
     console.log('error connection to MongoDB:', error.message)
   })
 
+
 // setup is now within a function
 const start = async () => {
   const app = express()
   const httpServer = http.createServer(app)
 
+  const wsServer = new WebSocketServer({
+    server: httpServer,
+    path: '/',
+  })
+  
+  const schema = makeExecutableSchema({ typeDefs, resolvers })
+  const serverCleanup = useServer({ schema }, wsServer);
+
   const server = new ApolloServer({
-    schema: makeExecutableSchema({ typeDefs, resolvers }),
-    plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
+    schema,
+    plugins: [
+      ApolloServerPluginDrainHttpServer({ httpServer }),
+      {
+        async serverWillStart() {
+          return {
+            async drainServer() {
+              await serverCleanup.dispose();
+            },
+          };
+        },
+      },
+    ],
   })
 
   await server.start()
